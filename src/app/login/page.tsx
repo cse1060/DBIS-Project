2c6ae64619d4da84fc7e6366a025b98db374d045
vashistha <vashistha.chaturvedi410@gmail.com>
2023-11-03 16:35:12 +0530

    added navbar and login signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,56 +4,84 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import SigninButton from "@/components/SigninButton";
+import "../../css/login.css";
+
 
 export default function LoginPage() {
-    const router = useRouter();
-    const [user, setUser] = useState({
-        email: "",
-        password: "",
-    })
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const onLoginup = async () => {
-        try {
-            const response = await axios.post("/api/users/login", user);
-            console.log(response.data);
-            if (response.data.isnew) {
-                router.push(`/profile/create/${response.data.username}`);
-            } else {
-                router.push(`/profile/${response.data.username}`);
-            }
-        } catch (error: any) {
-            console.log(error);
-        }
+  const onLoginup = async () => {
+    try {
+      const response = await axios.post("/api/users/login", user);
+      console.log(response.data);
+      if (response.data.isnew) {
+        router.push(`/profile/create/${response.data.username}`);
+      } else {
+        router.push(`/profile/${response.data.username}`);
+      }
+    } catch (error: any) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div>
-            <SigninButton type="login" />
-            <h1>Login</h1>
-            <hr />
-            <label htmlFor="email" >email: </label>
-            <input
-                type="text"
-                id="email"
-                value={user.email}
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                placeholder="email"
-            /><br />
-            <label htmlFor="password" >password: </label>
-            <input
-                type="password"
-                id="password"
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                placeholder="password"
-            /><br />
-            <button
-                onClick={onLoginup}
-            >Login here</button>
-            <br />
-            <Link href="/signup">Visit Signup Page</Link>
-            <hr></hr>
-            <Link href="/forgotPassword">Forgot Password ?</Link>
+  return (
+    <div id="container" className="container sign-in">
+      <SigninButton type="login" />
+      <div className="row">
+        <div className="col align-items-center flex-col sign-up"></div>
+        <div className="col align-items-center flex-col sign-in">
+          <div className="form-wrapper align-items-center">
+            <div className="form sign-in">
+              <div className="input-group">
+                <i className="bx bxs-user"></i>
+                <input
+                  type="text"
+                  id="email"
+                  value={user.email}
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                  placeholder="email"
+                />
+              </div>
+              <div className="input-group">
+                <i className="bx bxs-lock-alt"></i>
+                <input
+                  type="password"
+                  id="password"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  placeholder="password"
+                />
+              </div>
+              <button>Sign in</button>
+              <p>
+                <Link href="/forgotPassword">Forgot Password ?</Link>
+              </p>
+              <p>
+                <span>Don't have an account?</span>
+                <Link href="\signup">SignUp here</Link>
+              </p>
+            </div>
+          </div>
+          <div className="form-wrapper"></div>
         </div>
-    );
+      </div>
+      <div className="row content-row">
+        <div className="col align-items-center flex-col">
+          <div className="text sign-in">
+            <h2>Welcome</h2>
+          </div>
+          <div className="img sign-in"></div>
+        </div>
+      </div>
+      <video id="background-video" autoPlay loop muted>
+        <source src="video/videoplayback.webm" type="video/webm" />
+      </video>
+    </div>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import SigninButton from "@/components/SigninButton";
-
+import "../../css/login.css"
 export default function SignupPage() {
     const router = useRouter();
     const [user , setUser] = useState({
@@ -28,40 +28,111 @@ export default function SignupPage() {
         }
     }
 
-    return(
-        <div>
-            <SigninButton type = "signup"/>
-            <h1>{loading ? "Processing" : "Signup" }</h1>
-            <hr />
-            <label htmlFor="username" >username: </label>
-            <input 
-                type="text"
-                id="username"
-                value={user.username}
-                onChange={(e)=> setUser({...user , username : e.target.value})}
-                placeholder="username"
-            /><br />
-            <label htmlFor="email" >email: </label>
-            <input 
-                type="text"
-                id="email"
-                value={user.email}
-                onChange={(e)=> setUser({...user , email : e.target.value})}
-                placeholder="email"
-            /><br />
-            <label htmlFor="password" >password: </label>
-            <input 
-                type="password"
-                id="password"
-                value={user.password}
-                onChange={(e)=> setUser({...user , password : e.target.value})}
-                placeholder="password"
-            /><br />
-            <button
-                onClick={onSignup}
-            >Signup here</button>
-            <br />
-            <Link href="/login">Visit Login Page</Link>
+    return (
+      <div id="container" className="container sign-up">
+        <SigninButton type="signup" />
+            <h1>{loading ? "Processing" : "Signup"}</h1>
+            
+            
+        <div className="row">
+          <div className="col align-items-center flex-col sign-up">
+            <div className="form-wrapper align-items-center">
+              <div className="form sign-up">
+                <div className="input-group">
+                  <i className="bx bxs-user"></i>
+                  <input
+                    type="text"
+                    id="username"
+                    value={user.username}
+                    onChange={(e) =>
+                      setUser({ ...user, username: e.target.value })
+                    }
+                    placeholder="username"
+                  />
+                </div>
+                <div className="input-group">
+                  <i className="bx bx-mail-send"></i>
+                  <input
+                    type="text"
+                    id="email"
+                    value={user.email}
+                    onChange={(e) =>
+                      setUser({ ...user, email: e.target.value })
+                    }
+                    placeholder="email"
+                  />
+                </div>
+                <div className="input-group">
+                  <i className="bx bxs-lock-alt"></i>
+                  <input
+                    type="password"
+                    id="password"
+                    value={user.password}
+                    onChange={(e) =>
+                      setUser({ ...user, password: e.target.value })
+                    }
+                    placeholder="password"
+                  />
+                </div>
+                <button onClick={onSignup}>Signup here</button>
+                <p>
+                  <span>Already have an account?</span>
+                  <Link href="/login">Visit Login Page</Link>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="row content-row">
+          <div className="col align-items-center flex-col"></div>
+          <div className="col align-items-center flex-col">
+            <div className="img sign-up"></div>
+            <div className="text sign-up">
+              <h2>Join with us</h2>
+            </div>
+          </div>
+        </div>
+        <video id="background-video" autoPlay loop muted>
+          <source src="video/videoplayback.webm" type="video/webm" />
+        </video>
+      </div>
+      // <h1>Hello</h1>
     );
 }
+
+    
+
+    // <div>
+    //         <SigninButton type = "signup"/>
+    //         <h1>{loading ? "Processing" : "Signup" }</h1>
+    //         <hr />
+    //         <label htmlFor="username" >username: </label>
+    //         <input 
+    //             type="text"
+    //             id="username"
+    //             value={user.username}
+    //             onChange={(e)=> setUser({...user , username : e.target.value})}
+    //             placeholder="username"
+    //         /><br />
+    //         <label htmlFor="email" >email: </label>
+    //         <input 
+    //             type="text"
+    //             id="email"
+    //             value={user.email}
+    //             onChange={(e)=> setUser({...user , email : e.target.value})}
+    //             placeholder="email"
+    //         /><br />
+    //         <label htmlFor="password" >password: </label>
+    //         <input 
+    //             type="password"
+    //             id="password"
+    //             value={user.password}
+    //             onChange={(e)=> setUser({...user , password : e.target.value})}
+    //             placeholder="password"
+    //         /><br />
+    //         <button
+    //             onClick={onSignup}
+    //         >Signup here</button>
+    //         <br />
+    //         <Link href="/login">Visit Login Page</Link>
+    //     </div>
